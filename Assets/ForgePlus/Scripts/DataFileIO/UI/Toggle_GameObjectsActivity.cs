f4e67e5780b2fec6bf1ea293007d3e782827ff01
Richard Harrington <rlh@artleaping.com>
2020-04-25 17:11:21 -0700

    Added inspector foldout - used for "additional details" subsections

---
+++ Assets/ForgePlus/Scripts/DataFileIO/UI/Toggle_GameObjectsActivity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace ForgePlus.DataFileIO
+{
+    public class Toggle_GameObjectsActivity : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] targets = new GameObject[] { };
+
+        public void OnValueChanged(bool value)
+        {
+            foreach (var target in targets)
+            {
+                if (target)
+                {
+                    target.SetActive(value);
+                }
+                else
+                {
+                    Debug.LogError("This Toggle_GameObjectsActivity has a null GameObject reference!", this);
+                }
+            }
+        }
+    }
+}
