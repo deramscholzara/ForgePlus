51ff9469c0e524e19d6e9c520a826cfe25af964d
Richard Harrington <rlh@artleaping.com>
2020-05-17 14:43:33 -0700

    Version number increase and namespace cleanup.

--- Assets/ForgePlus/Scripts/ApplicationGeneral/UI/UIBlocking.cs
+++ Assets/ForgePlus/Scripts/ApplicationGeneral/UI/UIBlocking.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace ForgePlus.ApplicationGeneral
 {
